b81fe05b04014b2d9a49e2af0d681d9315718457
flujuju <julien.cattin@gmail.com>
2023-01-15 06:54:49 +0100

    handle player selection

--- script.js
+++ script.js
@@ -13,6 +13,29 @@ function getComputerChoice (arrayOfChoices) {
 console.log(getComputerChoice(OPTIONS));
 
 // prompt user to choose his play
+function getUserSelection() {
+    let userChoice;
+    let valid; 
+
+    
+    while (!valid) {
+        if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors"){
+            userChoice = prompt("Make your selection (rock, paper or scissors)");
+            userChoice = userChoice.toLowerCase();
+        }
+        
+        else {
+            valid = true;
+        }
+    }
+
+    return userChoice;
+    
+    
+}
+
+console.log(getUserSelection());
+
 // verify that user choice is valid
 // display who played what
 // define who won
