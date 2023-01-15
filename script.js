15d1ba42fd0d9b44949de29c96a4895fcc199dfb
flujuju <julien.cattin@gmail.com>
2023-01-15 14:48:40 +0100

    add game functions and 5-round-rule

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log("Hello");
 
 // let computer choose its play, randomly
 const OPTIONS = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice (arrayOfChoices) {
     let itemNumber = Math.floor(Math.random()* arrayOfChoices.length);
@@ -10,9 +12,10 @@ function getComputerChoice (arrayOfChoices) {
     return computerChoice;
 }
 
-console.log(getComputerChoice(OPTIONS));
+// console.log(getComputerChoice(OPTIONS));
 
 // prompt user to choose his play
+// verify that user choice is valid
 function getUserSelection() {
     let userChoice;
     let valid; 
@@ -34,10 +37,87 @@ function getUserSelection() {
     
 }
 
-console.log(getUserSelection());
+//console.log(getUserSelection());
 
-// verify that user choice is valid
-// display who played what
 // define who won
+function playRound(computerSelection, playerSelection) {
+    switch(computerSelection) {
+        case "rock":
+            if(playerSelection == "rock") {
+                console.log("It's a draw");
+            }
+            else if (playerSelection == "paper") {
+                console.log("player wins !");
+                playerScore += 1;
+            }
+            else {
+                console.log("Computer wins !");
+                computerScore += 1;
+            }
+            break;
+
+        case "paper":
+            if(playerSelection == "paper") {
+                console.log("It's a draw");
+            }
+            else if (playerSelection == "scissors") {
+                console.log("player wins !");
+                playerScore += 1;
+            }
+
+            else {
+                console.log("Computer wins !");
+                computerScore += 1;
+            }
+            break;
+
+        case "scissors":
+            if(playerSelection == "scissors") {
+                console.log("It's a draw");
+            }
+            else if (playerSelection == "rock") {
+                console.log("player wins !");
+                playerScore += 1;
+            }
+            else {
+                console.log("Computer wins !");
+                computerScore += 1;
+            }
+            break;
+    }
+
+}
+
+function displayScore(playerScore, computerScore){
+    console.log(`SCORE - player : ${playerScore} // computer : ${computerScore}`);
+}
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let computerChoice = getComputerChoice(OPTIONS);
+        console.log(computerChoice);
+        let userSelection = getUserSelection();
+        playRound(computerChoice, userSelection);
+        displayScore(playerScore, computerScore);
+
+
+    }
+
+    console.log(`Game is finished, final score is :`);
+    displayScore(playerScore, computerScore);
+
+
+    
+
+}
+
+game()
+
+
+
+// display who played what
+
+
+
 // calculate and display score
 // define winner in the best of five fashion
